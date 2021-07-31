fa88c8c799674a6167d471d34a84a5f0a84ec942
Daniel Rodrigues <daniel.rodrigues.cavadas@gmail.com>
2021-07-31 19:33:34 +0100

    Added logger middleware and file writer

--- gocontact-backend/app.js
+++ gocontact-backend/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const routes = require('./routes/routes');
+const logger = require('./middlewares/logger');
 
 const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(logger);
 
 app.use(routes);
 
