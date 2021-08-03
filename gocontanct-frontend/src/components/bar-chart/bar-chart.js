2b6bc0215f2be2debbc08603aed00e65fec3c875
Daniel Rodrigues <daniel.rodrigues.cavadas@gmail.com>
2021-08-03 20:35:29 +0100

    Implemented the table, maked some changes on CSS and on chart-bar logic, changed and added README files

--- gocontanct-frontend/src/components/bar-chart/bar-chart.js
+++ gocontanct-frontend/src/components/bar-chart/bar-chart.js
@@ -1,51 +1,42 @@
 import {useState, useEffect, useLayoutEffect} from 'react'
 import './bar-chart.css';
-import { TextField } from '@material-ui/core';
-import Button from '@material-ui/core/Button';
 import {getCities} from '../../services/api';
-import Alert from '@material-ui/lab/Alert';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
 import Chart from "react-google-charts";
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 
-function BarChart() {
+function BarChart({data:data}) {
     const [dataChart, setDataChart] = useState([['Cidade', 'Temperatura']]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        getCities().then(response=>{
-            console.log(response.data.cities);
-            response.data.cities.forEach(element => {
-                let auxArray = [element.name, element.main.temp];
-                console.log(auxArray);
-                dataChart.push(auxArray);
-                setDataChart(dataChart);
-            });
-
-        }).catch(err=>{
-            console.log(err);
-        })
-    });
+        setLoading(true);
+        let temporaryArray = dataChart; //[];
+        data.forEach(element => {
+            let auxArray = [element.name, element.main.temp];
+            temporaryArray.push(auxArray);
+        });
+        setDataChart([...temporaryArray]);
+        setLoading(false);
+    },[data]);
 
     return (
         <div className='PrincipalContainerChart' >
+            { !loading ?
             <Chart
-                width={'500px'}
+                width={'1000px'}
                 height={'300px'}
                 chartType="Bar"
                 loader={<CircularProgress />}
                 data={dataChart}
                 options={{
-                    // Material design options
                     chart: {
                     title: 'Temperatura nas cidades',
                     subtitle: '',
                     },
                 }}
-                // For tests
-                rootProps={{ 'data-testid': '2' }}
-            />
+            /> : <CircularProgress></CircularProgress>
+            }
         </div>
     );
 }
